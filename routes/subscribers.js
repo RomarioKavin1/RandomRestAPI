bed7138d9e85745f07844f362a5e938b2d7a5834
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2023-04-25 23:32:09 +0530

    routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Subscriber = require('../models/subscriber');
+const subscriber = require('../models/subscriber');
 
 module.exports = router;
 
@@ -13,8 +14,8 @@ router.get('/',async(req,res)=>{
     }
 });
 
-router.get('/:id',(req,res)=>{
-    res.send(req.params.id);
+router.get('/:id',getSubscriber,(req,res)=>{
+    res.send(res.subscriber);
 });
 
 router.post('/',async (req,res)=>{
@@ -30,9 +31,32 @@ router.post('/',async (req,res)=>{
     }
     
 });
-router.patch('/:id',(req,res)=>{
+router.patch('/:id',getSubscriber,(req,res)=>{
     
 });
-router.delete('/:id',(req,res)=>{
-    
+router.delete('/:id',getSubscriber, async(req,res)=>{
+    try{
+        
+        await res.subscriber.remove()
+        res.json({ message :'Deleted'})
+    }
+    catch(err){
+        res.status(500).json({message:err.message})
+    }
 });
+
+async function getSubscriber(req,res,next){
+    let subscriber
+    try{
+        subscriber = await Subscriber.findById(req.params.id);
+        if(subscriber == null){
+            return res.status(404).json({message:'Cannot find Subscriber'})
+
+        }
+    }
+    catch(err){
+        return res.status(500).json({message:err.message  })
+    }
+    res.subscriber=subscriber;
+    next();
+}
